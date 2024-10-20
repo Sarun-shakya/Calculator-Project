bb0176dd6e8dfe3d3193fd9a2c4e47256974610d
Sarun-shakya <sarunshakya414@gmail.com>
2024-10-20 16:21:14 +0545

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const display = document.getElementById("display");
+
+function addToDisplay(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculateResult() {
+        display.value = eval(display.value);
+}
